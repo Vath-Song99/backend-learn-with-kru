be6accaf95cf543c915bb9a3f9fe139828e33191
smoeury-songvate <vatgaming287@gmail.com>
2024-05-02 22:21:21 +0700

    somthing new

--- auth/src/middlewares/errorsHandler.ts
+++ auth/src/middlewares/errorsHandler.ts
@@ -1,17 +1,21 @@
-import { Request, Response, NextFunction} from 'express'
-import {  BaseCustomError } from '../error/base-custom-error'
+import { Request, Response, NextFunction } from "express";
+import { BaseCustomError } from "../error/base-custom-error";
 
-
-export const errorHandler = async (error: Error, _req: Request, res: Response, _next: NextFunction) =>{
-    if(error instanceof BaseCustomError){
+export const errorHandler = async (
+  error: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  if (error instanceof BaseCustomError) {
     const status = error.statusCode;
-        res.status(status).json({
-            success: false,
-            error: {
-                message: error.message,
-                statusCode: status
-              }
-        })
-    }
-    _next()
-}
+    res.status(status).json({
+      success: false,
+      errors: {
+        message: error.message,
+        statusCode: status,
+      },
+    });
+  }
+  _next();
+};
